7e5ee0393c2913b921131fbbcca8f5f8da5640bd
maoqiuqiu <52489833+yf0522@users.noreply.github.com>
2026-09-21 09:46:41 +0800

    feat: add global shortcut to open clipboard

--- EdgeMark/Core/Shortcuts/ShortcutManager.swift
+++ EdgeMark/Core/Shortcuts/ShortcutManager.swift
@@ -6,28 +6,28 @@ import OSLog
 final class ShortcutManager {
     static let shared = ShortcutManager()
 
-    private var hotKeyRef: EventHotKeyRef?
+    private var togglePanelHotKeyRef: EventHotKeyRef?
+    private var openClipboardHotKeyRef: EventHotKeyRef?
     private var eventHandler: EventHandlerRef?
-    private var toggleAction: (() -> Void)?
+
+    private weak var panelController: SidePanelController?
 
     private init() {}
 
     func setup(panelController: SidePanelController) {
-        toggleAction = { [weak panelController] in
-            panelController?.togglePanel()
-        }
+        self.panelController = panelController
         registerShortcuts()
 
         NotificationCenter.default.addObserver(
             self,
             selector: #selector(shortcutSettingsChanged),
             name: .shortcutSettingsChanged,
-            object: nil,
+            object: nil
         )
     }
 
     @objc private func shortcutSettingsChanged() {
-        Log.shortcuts.info("[ShortcutManager] re-registering shortcut")
+        Log.shortcuts.info("[ShortcutManager] re-registering shortcuts")
         unregisterShortcuts()
         registerShortcuts()
     }
@@ -35,69 +35,127 @@ final class ShortcutManager {
     // MARK: - Register / Unregister
 
     private func registerShortcuts() {
-        guard let shortcut = ShortcutSettings.shared.togglePanelShortcut else {
-            Log.shortcuts.info("[ShortcutManager] no shortcut configured, skipping registration")
-            return
+        installEventHandler()
+
+        if let shortcut = ShortcutSettings.shared.togglePanelShortcut {
+            register(
+                shortcut,
+                id: 1,
+                ref: &togglePanelHotKeyRef,
+                label: "toggle panel"
+            )
         }
 
-        // Signature: 'EMRK' (EdgeMark)
-        let hotKeyID = EventHotKeyID(signature: OSType(0x454D_524B), id: 1)
+        if let shortcut = ShortcutSettings.shared.openClipboardShortcut {
+            register(
+                shortcut,
+                id: 2,
+                ref: &openClipboardHotKeyRef,
+                label: "open clipboard"
+            )
+        }
+    }
 
-        var ref: EventHotKeyRef?
+    private func register(
+        _ shortcut: KeyboardShortcut,
+        id: UInt32,
+        ref: inout EventHotKeyRef?,
+        label: String
+    ) {
+        let hotKeyID = EventHotKeyID(
+            signature: OSType(0x454D_524B),
+            id: id
+        )
+
         let status = RegisterEventHotKey(
             UInt32(shortcut.keyCode),
             shortcut.modifiers,
             hotKeyID,
             GetEventDispatcherTarget(),
             0,
-            &ref,
+            &ref
         )
 
         if status == noErr {
-            hotKeyRef = ref
-            installEventHandler()
-            Log.shortcuts.info("[ShortcutManager] registered hotkey")
+            Log.shortcuts.info("[ShortcutManager] registered \(label, privacy: .public)")
         } else {
-            Log.shortcuts.error("[ShortcutManager] failed to register hotkey (status: \(status))")
+            Log.shortcuts.error(
+                "[ShortcutManager] failed to register \(label, privacy: .public) (status: \(status))"
+            )
         }
     }
 
     private func installEventHandler() {
+        guard eventHandler == nil else { return }
+
         var eventType = EventTypeSpec(
             eventClass: OSType(kEventClassKeyboard),
-            eventKind: UInt32(kEventHotKeyPressed),
+            eventKind: UInt32(kEventHotKeyPressed)
         )
 
         InstallEventHandler(
             GetEventDispatcherTarget(),
             { _, event, userData -> OSStatus in
                 guard let userData else { return OSStatus(eventNotHandledErr) }
-                let manager = Unmanaged<ShortcutManager>.fromOpaque(userData).takeUnretainedValue()
-                manager.handleHotKeyEvent(event)
-                return noErr
+                let manager = Unmanaged<ShortcutManager>
+                    .fromOpaque(userData)
+                    .takeUnretainedValue()
+                return manager.handleHotKeyEvent(event)
             },
             1,
             &eventType,
             Unmanaged.passUnretained(self).toOpaque(),
-            &eventHandler,
+            &eventHandler
         )
     }
 
-    private func handleHotKeyEvent(_: EventRef?) {
-        Log.shortcuts.debug("[ShortcutManager] hotkey pressed")
-        toggleAction?()
+    private func handleHotKeyEvent(_ event: EventRef?) -> OSStatus {
+        guard let event else { return OSStatus(eventNotHandledErr) }
+
+        var hotKeyID = EventHotKeyID()
+        let status = GetEventParameter(
+            event,
+            EventParamName(kEventParamDirectObject),
+            EventParamType(typeEventHotKeyID),
+            nil,
+            MemoryLayout<EventHotKeyID>.size,
+            nil,
+            &hotKeyID
+        )
+        guard status == noErr else { return status }
+
+        switch hotKeyID.id {
+        case 1:
+            Log.shortcuts.debug("[ShortcutManager] toggle-panel hotkey pressed")
+            panelController?.togglePanel()
+            return noErr
+        case 2:
+            Log.shortcuts.debug("[ShortcutManager] clipboard hotkey pressed")
+            AppNavigation.shared.showClipboard()
+            panelController?.showPanel()
+            return noErr
+        default:
+            return OSStatus(eventNotHandledErr)
+        }
     }
 
     private func unregisterShortcuts() {
-        Log.shortcuts.debug("[ShortcutManager] unregistered hotkey")
-        if let hotKeyRef {
-            UnregisterEventHotKey(hotKeyRef)
-            self.hotKeyRef = nil
+        if let togglePanelHotKeyRef {
+            UnregisterEventHotKey(togglePanelHotKeyRef)
+            self.togglePanelHotKeyRef = nil
         }
+
+        if let openClipboardHotKeyRef {
+            UnregisterEventHotKey(openClipboardHotKeyRef)
+            self.openClipboardHotKeyRef = nil
+        }
+
         if let eventHandler {
             RemoveEventHandler(eventHandler)
             self.eventHandler = nil
         }
+
+        Log.shortcuts.debug("[ShortcutManager] unregistered shortcuts")
     }
 
     deinit {
